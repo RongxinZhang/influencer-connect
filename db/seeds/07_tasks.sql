7aaa08d290e900ee6e6a40f670561bdf74a96408
whyttte <fogboruche@yahoo.com>
2020-09-27 01:19:41 +0000

    updating the change on tasks.sql

--- db/seeds/07_tasks.sql
+++ db/seeds/07_tasks.sql
@@ -5,4 +5,4 @@ VALUES
 INSERT INTO tasks
   (campaign_id, user_type, status, description, start_date)
 VALUES
-  (3, 'influencer', 'on going', 'Coca-Cola Task Description', '2020-09-26');
+  (1, 'influencer', 'on going', 'Coca-Cola Task Description', '2020-09-26');
