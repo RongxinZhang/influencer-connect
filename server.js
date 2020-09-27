11fa2545278c086b4ca64ec852700cadae19a4ec
Rongxin <mrrongxin@gmail.com>
2020-09-27 15:54:35 +0000

    Making queries

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-const ENV = require('./src/environment');
+const ENV = require('./lib/environment');
+const db = require("./lib/db");
+
+const text = 'SELECT * FROM users WHERE first_name=$1;'
+const values = ['Balky']
+
+db.query(text, values)
+  .then(res => {
+    console.log(res.rows)
+  })
+  .catch(err => console.error(err.stack))
