79f5d44c73b2f731faff5628cf8355df300d4fcb
Balkaran <balkaranjaswal@gmail.com>
2020-09-28 23:24:48 +0000

    Working server.js

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ const cors = require('cors');
 //Separate Routes for each Resource
 const campaignsRoutes = require("./lib/routes/campaigns");
 // const campaignUsersRoutes = require("./lib/routes/campaignUsers");
-const campaignTasks = require("./lib/routes/campaignTasks");
-const createTask = require("./lib/routes/createTask");
-const updateStatusOfTask = require("./lib/routes/updateStatusOfTask");
-const campaignMessages = require("./lib/routes/campaignMessages");
-const createMessage = require("./lib/routes/createMessage");
-const deleteCampaign = require("./lib/routes/deleteCampaign");
+// const campaignTasks = require("./lib/routes/campaignTasks");
+// const createTask = require("./lib/routes/createTask");
+// const updateStatusOfTask = require("./lib/routes/updateStatusOfTask");
+// const campaignMessages = require("./lib/routes/campaignMessages");
+// const createMessage = require("./lib/routes/createMessage");
+// const deleteCampaign = require("./lib/routes/deleteCampaign");
 
 
 const port = process.env.PORT || 3000;
@@ -25,15 +25,15 @@ app.use(express.json());
 
 
 //Mounting all resource routes
-app.use("/campaigns", campaigns)
-app.use("/campaigns", campaignsRoutes.getAllUsers(db));
+app.use("/campaigns", campaignsRoutes)
+// app.use("/campaigns", campaignsRoutes.getAllUsers(db));
 // app.use("/campaigns", campaignUsersRoutes.getAllUsers(db));
-app.use("/campaignTasks", campaignTasks.getAllTasks(db));
-app.use("/createTask", createTask.createTask(db));
-app.use("/updateStatusOfTask", updateStatusOfTask.updateStatusOfTask(db));
-app.use("/campaignMessages", campaignMessages.getCampaignMessages(db));
-app.use("/createMessage", createMessage.createMessage(db));
-app.use("/deleteCampaign", deleteCampaign.deleteCampaign(db));
+// app.use("/campaignTasks", campaignTasks.getAllTasks(db));
+// app.use("/createTask", createTask.createTask(db));
+// app.use("/updateStatusOfTask", updateStatusOfTask.updateStatusOfTask(db));
+// app.use("/campaignMessages", campaignMessages.getCampaignMessages(db));
+// app.use("/createMessage", createMessage.createMessage(db));
+// app.use("/deleteCampaign", deleteCampaign.deleteCampaign(db));
 
 
 
