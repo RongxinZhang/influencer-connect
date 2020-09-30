1d8d469be1d69e08de22945980519f034cb9653c
Rongxin Zhang <mrrongxin@gmail.com>
2020-09-30 10:08:17 -0700

    Rongxin: Connect getTasks Controller

--- lib/db/seeds/all.sql
+++ lib/db/seeds/all.sql
@@ -69,10 +69,14 @@ INSERT INTO tasks
   (campaign_id, user_type, status, description, start_date)
 VALUES
   (1, 'influencer', 'on going', 'Nike Task Description', '2020-09-26'),
-  (2, 'influencer', 'on going', 'Coca-Cola Task Description', '2020-09-26'),
-  (3, 'influencer', 'on going', 'Mcdonalds Task Description', '2020-09-27'),
-  (4, 'influencer', 'on going', 'OVO Sound Task Description', '2020-09-27'),
-  (5, 'influencer', 'on going', 'Square Enix Task Description', '2020-09-27');
+  (1, 'influencer', 'on going', 'Coca-Cola Task Description', '2020-09-26'),
+  (1, 'influencer', 'on going', 'Mcdonalds Task Description', '2020-09-27'),
+
+  (1, 'brand', 'on going', 'Coca-Cola Task Description', '2020-09-26'),
+  (1, 'brand', 'on going', 'Mcdonalds Task Description', '2020-09-27'),
+  
+  (2, 'influencer', 'on going', 'OVO Sound Task Description', '2020-09-27'),
+  (2, 'influencer', 'on going', 'Square Enix Task Description', '2020-09-27');
 
 
 
