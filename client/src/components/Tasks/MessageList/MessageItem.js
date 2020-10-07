621d0ae488af813c035e6090d8fe1f6d0ec74006
Rongxin Zhang <mrrongxin@gmail.com>
2020-10-06 18:12:31 -0700

    update message

--- client/src/components/Tasks/MessageList/MessageItem.js
+++ client/src/components/Tasks/MessageList/MessageItem.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
+import Moment from "react-moment";
 import "./MessageItem.scss";
 
 const MessageItem = function (props) {
-  console.log("this is messageitem: ", props);
   return (
     <main>
       <li className="message-list-item" data-message-id={props.campaignId}>
         <div className="message_list_item_header">
-          <div>{props.name}</div>
-          <div>{props.content}</div>
-          <div className="message_list_item_date">{Date(Date.now(props.date))}</div>
+          <div>{props.name || "balky"}</div>
+          <div className="message_list_item_date">
+            <Moment format="MMM DD, YYYY">{Date(props.content)}</Moment>
           </div>
+        </div>
+        <div>{props.content}</div>
       </li>
     </main>
   );
