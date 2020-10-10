791cbac8da64e89b7d74149b2b9b75db95b36467
Rongxin Zhang <mrrongxin@gmail.com>
2020-10-10 10:46:47 -0700

    Remove unused db files

--- server/lib/db/queries/01_all_inflencer_all_campaigns_manager_owns.sql
+++
@@ -1,8 +0,0 @@
-select * from influencers
-	inner join campaigns on influencers.id = campaigns.influencer_id
-	inner join campaign_details on campaign_details.id = campaigns.campaign_detail_id
-	inner join brands on brands.id = campaign_details.brand_id
-	inner join brand_managers on brand_managers.brand_id = brands.id
-	inner join users on users.id = brand_managers.user_id
-where users.id=2
-  
